538fd616b5754e9e8c5ecae3ef4e47570ac7fa27
Leonardo Silva <f.leobrito@gmail.com>
2020-12-17 00:18:41 -0300

    :lipstick: Adding styled-components

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background: #121214;
+    color: #e1e1e6;
+  }
+`
